605168bff577c1f198911da9757348249cedb921
omarammura <omarammoralm10@gmail.com>
2024-03-15 20:14:05 +0300

    Update hello message in route_test.go

--- testing/route_test.go
+++ testing/route_test.go
@@ -17,5 +17,5 @@ func TestHelloReturns200(t *testing.T) {
 	r.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
-	assert.JSONEq(t, `{"message": "Hello,World!"}`, w.Body.String())
+	assert.JSONEq(t, `{"message": "Not found!"}`, w.Body.String())
 }
