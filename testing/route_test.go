0a5d3f63cfccdda4ed701263784c0fde950bd602
omarammura <omarammoralm10@gmail.com>
2024-03-15 20:18:13 +0300

    Update response code and message in route_test.go

--- testing/route_test.go
+++ testing/route_test.go
@@ -16,6 +16,6 @@ func TestHelloReturns200(t *testing.T) {
 
 	r.ServeHTTP(w, req)
 
-	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, 404, w.Code)
 	assert.JSONEq(t, `{"message": "Not found!"}`, w.Body.String())
 }
